089a715f0fc913db0e65be010f16630ed1183b59
Adarsh Rajput <adarshrajput2931@gmail.com>
2026-03-30 23:08:18 +0530

    Add Render deployment config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chatify-01-kpwc.onrender.com/api",
+      : "/api",
   withCredentials: true,
 });
