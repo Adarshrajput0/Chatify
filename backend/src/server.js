34530c68c5f6fde89d335679f52399516330ad25
Adarsh Rajput <adarshrajput2931@gmail.com>
2026-04-03 12:33:16 +0530

    Final changes3

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import cors from "cors";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.router.js";
@@ -11,6 +12,8 @@ import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const PORT = process.env.PORT || 5002;
 
@@ -22,11 +25,21 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../../frontend", "dist")));
+
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+//   });
+// }
+
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend", "dist")));
+  const frontendDist = path.join(__dirname, "../../../frontend/dist");
+
+  app.use(express.static(frontendDist));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(frontendDist, "index.html"));
   });
 }
 
